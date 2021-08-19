e05f2e5abb123236a682968dc47212f7a92d10eb
Julian Vargas <julian1245@hotmail.com>
2021-08-19 09:25:50 -0500

    Update config.js
    config tu heroku

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'mVXtFHdb1uRBt3jOOR33xa1J6jheJ0Me2iL161se3xbn6Y8O1RhYBj2ZxjKjYRk',
 
-  LISTEN_PORT: process.env.LISTEN_PORT || 4000,
+  LISTEN_PORT: process.env.PORT || 4000,
 
   HERE_API_HEY: process.env.HERE_API_HEY || 'X-G752YcewgRWGHsPWV3zwXRCycXh6S6aLXwEhMdr_w'
 };
